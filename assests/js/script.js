a6b19eaeab7ee338620f51319a96ceb8e5f18ebc
Harith Albadry <harith.albadry@gmail.com>
2021-03-21 18:03:29 +0000

    fetch API now working

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,46 +1,45 @@
-// variabvles 
-var cityEl= document.querySelector("#cityName")
-var submitEl= document.querySelector("#submit_btn")
-var APPID="5529346d7fb490d9f9af910d01a074f0"
-
-
-
+// variabvles
+var cityEl = document.querySelector("#cityName")
+var submitEl = document.querySelector("#submit_btn")
+var APPID = "5529346d7fb490d9f9af910d01a074f0"
+var palyEl=""
+var testEl=document.querySelector("#test")
 // functions
 
-function handleSearch(event){
-    event.preventDefault();
+// function handleSearch(event){
+//     event.preventDefault();
 
-    var cityName = document.querySelector("cityName").value
+//     var cityName = document.querySelector("cityName").value
 
-    if (!cityName){
-        console.error("you need a valid city name please");
-    }
-    // createURL()
-    getData();
-}
+//     if (!cityName){
+//         console.error("you need a valid city name please");
+//     }
+//     // createURL()
+//     getData();
+// }
 
 // function createURL(){
 //     var cityURL="api.openweathermap.org/data/2.5/weather?id="+ cityName +"&appid=" + APPID;
 //     console.log(cityURL);
 // }
 
-
-function getData (){
-
-    fetch (https://api.openweathermap.org/data/2.5/onecall?lat=53.5&lon=2.6&appid=5529346d7fb490d9f9af910d01a074f0 
-    ).then(function(response){
-        if(!response.ok){
-            throw response.json();
-        }
-        return response.json();
-    })
-.then(function(response){
-    console.log("response is: " + response)
-}) 
+function getData() {
+	fetch(
+		"https://api.openweathermap.org/data/2.5/onecall?lat=53.5&lon=2.6&appid=5529346d7fb490d9f9af910d01a074f0"
+	)
+		.then(function (response) {
+			return response.json();
+		})
+		.then(function (data) {
+			console.log("response is: " + data);
+            console.log(typeof(data));
+            console.log(data.current.weather[0].description);
+            palyEl=data.current.weather[0].description;
+            testEl.textContent=(palyEl)
+            
+		})
 }
 
-
-// button functions 
-submitEl.addEventListener('submit', handleSearch);
-
-
+// button functions
+// submitEl.addEventListener('submit', handleSearch);
+getData()
