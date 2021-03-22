32ab3eb51a54e15b3d611e733f15191b462f1b89
Harith Albadry <harith.albadry@gmail.com>
2021-03-22 16:28:23 +0000

    list of cities from local storage now added

--- assests/js/script.js
+++ assests/js/script.js
@@ -2,49 +2,82 @@
 var cityEl = document.querySelector("#cityName")
 var submitEl = document.querySelector("#submit_btn")
 var APPID = "5529346d7fb490d9f9af910d01a074f0"
-var palyEl=""
-var testEl=document.querySelector("#test")
-var tempEl=document.querySelector("#temp");
-var temp=""
-var humEl=document.querySelector("#humidity");
-var humidity=""
-var windEl=document.querySelector("#wind");
-var wind=""
-var uvEl=document.querySelector("#uv");
-var uvi="";
-// var citylistEl=
-var listOfCities=[];
+var palyEl = ""
+var testEl = document.querySelector("#test")
+var tempEl = document.querySelector("#temp")
+var temp = ""
+var humEl = document.querySelector("#humidity")
+var humidity = ""
+var windEl = document.querySelector("#wind")
+var wind = ""
+var uvEl = document.querySelector("#uv")
+var uvi = ""
+var citylistEl = document.querySelector("#list_of_cities")
+var listOfCities = []
+var cities = []
 // functions
 
-function handleSearch(event){
-    event.preventDefault();
+function handleSearch(event) {
+	event.preventDefault()
 
-    var cityName = cityEl.value.trim();
-    console.log(cityName);
-    if (!cityName){
-        console.error("you need a valid city name please");
-    }
-    createURL();
-    citylist(cityName);
-    
-    // getData();
+	var cityName = cityEl.value.trim()
+	console.log(cityName)
+	if (!cityName) {
+		console.error("you need a valid city name please")
+	}
+	createURL();
+	citylist(cityName);
+    getStoreCities();
+    writeList();
+	// getData();
 }
 
-function createURL(){
-    var cityURL="api.openweathermap.org/data/2.5/weather?id="+ cityName.value +"&appid=" + APPID;
-    console.log(cityURL);
+function createURL() {
+	var cityURL =
+		"api.openweathermap.org/data/2.5/weather?id=" +
+		cityName.value +
+		"&appid=" +
+		APPID
+	console.log(cityURL)
 }
 
+function citylist(cityName) {
+	listOfCities.push(cityName)
+	console.log("list of cities include:: " + listOfCities)
+	localStorage.setItem(
+		"visitedCities",
+		JSON.stringify(listOfCities)
+	)
+}
 
-function citylist(cityName){
-listOfCities.push(cityName);
-console.log("list of cities include:: " + listOfCities)
-localStorage.setItem("visitedCities", JSON.stringify(listOfCities));
+function getStoreCities() {
+	var storedCities = JSON.parse(
+		localStorage.getItem("visitedCities")
+	)
+	if (storedCities !== null) {
+		cities = storedCities
+	}
+}
+
+function writeList() {
+	// Clear todoList element and update todoCountSpan
+	citylistEl.innerHTML = ""
+
+	// Render a new li for each todo
+	for (var i = 0; i < cities.length; i++) {
+		var city = cities[i]
+
+		var li = document.createElement("li")
+		li.textContent = city
+		li.setAttribute("data-index", i)
+
+		citylistEl.appendChild(li)
+	}
 }
 
 // function getData() {
-	// fetch(
-	// 	"https://api.openweathermap.org/data/2.5/onecall?lat=53.5&lon=2.6&appid=5529346d7fb490d9f9af910d01a074f0"
+// fetch(
+// 	"https://api.openweathermap.org/data/2.5/onecall?lat=53.5&lon=2.6&appid=5529346d7fb490d9f9af910d01a074f0"
 // 	// )
 // 		.then(function (response) {
 // 			return response.json();
@@ -71,5 +104,5 @@ localStorage.setItem("visitedCities", JSON.stringify(listOfCities));
 // }
 
 // button functions
-submitEl.addEventListener('click', handleSearch);
+submitEl.addEventListener("click", handleSearch)
 // getData()
